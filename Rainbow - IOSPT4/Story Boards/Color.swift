beeae24a29d976e093bce0239a3a7a349e03c876
David Williams <burmesedragon@att.net>
2019-11-12 19:31:43 -0800

    Set up initial views

---
+++ Rainbow - IOSPT4/Story Boards/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rainbow - IOSPT4
+//
+//  Created by David Williams on 11/12/19.
+//  Copyright © 2019 david williams. All rights reserved.
+//
+
+import UIKit
+
+struct MyColor {
+    let name: String
+    let color: UIColor
+}

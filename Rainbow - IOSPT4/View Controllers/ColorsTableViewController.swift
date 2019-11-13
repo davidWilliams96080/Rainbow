5df04d960998015cfd7a50239eeae06027ba0e5e
David Williams <burmesedragon@att.net>
2019-11-12 20:36:34 -0800

    Got colors working on ColorDetail View

--- Rainbow - IOSPT4/View Controllers/ColorsTableViewController.swift
+++ Rainbow - IOSPT4/View Controllers/ColorsTableViewController.swift
@@ -79,14 +79,17 @@ class ColorsTableViewController: UITableViewController {
      }
      */
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showColorSegue" {
+            guard let indexPath =
+                tableView.indexPathForSelectedRow,
+                let colorDetailVC = segue.destination as?
+                ColorDetailViewController else { return }
+            
+            let cellColor = colors[indexPath.row]
+            
+            colorDetailVC.cellColor = cellColor
+        }
+    }
     
 }
